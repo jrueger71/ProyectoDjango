4df596e7b8bb588298d09d4c98ce934c99947fd0
Jorge Rueger <jorgerueger@Jorges-MacBook-Pro.local>
2024-05-28 19:59:14 -0400

    cambios segundo dia

--- ProyectoFinal/ProyectoFinal/contactaapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('contact/', views.contact_view, name='contact'),
-    path('success/', views.success_message, name='success_message'),
-]
